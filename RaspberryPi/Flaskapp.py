e97b4f59cdf3630b883013c060685b67526717cf
Asark1 <arshdeep276@gmail.com>
2025-10-30 18:47:24 -0500

    this dont work

--- RaspberryPi/Flaskapp.py
+++ RaspberryPi/Flaskapp.py
@@ -256,8 +256,7 @@ def receive_command():
         elif cmd == "pid-toggle":
             blimp_data["pid-toggle"] = 1 - blimp_data["pid-toggle"]  # toggle between 0 and 1
             if blimp_data["pid-toggle"] == 0:
-                control_commands["backleft-motor"] = 0
-                control_commands["backright-motor"] = 0
+                control_commands["back-motors"] = 0
         return "OK", 200
     return "No command received", 400
 
